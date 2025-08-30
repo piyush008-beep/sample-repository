976360867aac64a215efb55f14ae68f8d82668c6
piyush008-beep <almightypush008@icloud.com>
2025-08-31 00:55:49 +0530

    coding
    days of code

---
+++ day10.c
@@ -0,0 +1,22 @@
+//Write a program to classify a triangle as Equilateral, Isosceles, or Scalene based on its side lengths.
+
+
+#include <stdio.h>
+int main()
+{int a,b,c; 
+    printf("enter the first angle 0f triangle");
+    scanf("%d",&a);
+     printf("enter the second angle triangle");
+    scanf("%d",&b);
+     printf("enter the third angle triangle");
+    scanf("%d",&c);
+    if(a==b && b==c)
+     {printf("triangle is equilateral");}
+else if(a==b || b==c)
+  {printf("triangle is isoceles");}
+else
+  {printf("triangle is obtuse");}
+return 0;
+
+
+}
---
+++ day9.1.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{int x;
+  pow(x,2)+6*x+8==0
+  
