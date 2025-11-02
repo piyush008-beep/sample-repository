a186a2f58ed5e8e45bc0cbb00bda649e93fd8194
piyush008-beep <almightypush008@icloud.com>
2025-11-02 23:05:44 +0530

    da

---
+++ day55.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int mCount = n / 2;
+    int found = 0;
+
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+
+        
+        for (int j = 0; j < n; j++) {
+            if (nums[j] == nums[i])
+                count++;
+        }
+
+        
+        if (count > mCount) {
+            printf("Majority element: %d\n", nums[i]);
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found)
+        printf("-1\n");
+
+    return 0;
+}
