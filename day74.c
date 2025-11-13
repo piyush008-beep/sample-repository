495a276cfa545f51037349dbc89b16e79147ff89
piyush008-beep <almightypush008@icloud.com>
2025-11-14 00:40:11 +0530

    done

--- day74.c
+++ day74.c
@@ -1,19 +1,24 @@
 /*Take two filenames from the user – a source file and a destination file. Copy all the content from the source file to the destination 
 file using fgetc() and fputc().*/
 #include <stdio.h>
-int mainI(){
-    char intro[50];
-    FILE *fp;
-    FILE *fp1;
-    fp=fopen("source.txt","r");
-    fp1=fopen("destination.txt","w");
-    if( fp==NULL || fp1==NULL)
-    {printf("error file not present !!!");}
-    fgetc(intro,fp);
-    fputc(intro,fp1);
+
+int main() {
+    FILE *fp = fopen("source.txt", "r");
+    FILE *fp1 = fopen("destination.txt", "w");
+    char ch;
+
+    while (1) {
+        ch = fgetc(fp);
+        if (feof(fp))
+            break;
+        fputc(ch, fp1);
+    }
+
     fclose(fp);
     fclose(fp1);
-    
-
-
+    printf("Copied successfully using char!\n");
+    return 0;
 }
+
+
+
