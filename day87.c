c475a9187e34f7936c70e55f1a7d25ac289209fb
piyush008-beep <almightypush008@icloud.com>
2025-11-15 20:12:45 +0530

    done

---
+++ day87.c
@@ -0,0 +1,36 @@
+/*Create an enum for user roles 
+(ADMIN, USER, GUEST) and display
+ messages based on role.*/
+
+#include <stdio.h>
+enum roles{
+    ADMIN,USER,GUEST
+};
+typedef struct assign{
+    enum roles a;
+}assign;
+int main(){
+    struct assign display;
+    printf("enter the number 0-admin,1-user,2-guest \n");
+    scanf("%d",&display.a);
+    switch (display.a) {
+        case GUEST:
+        printf("welcome guest!!!");
+        break;
+        case USER:
+        printf("PLEASE DO YOUR WORK!!");
+        break;
+        case ADMIN:
+        printf("HELLO ADMIN!!");
+        break; default:
+            printf("Invalid light value entered.\n");
+            break;
+    }
+
+     return 0;
+
+    }
+
+
+   
+
