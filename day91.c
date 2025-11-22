5fe7d718d31d73dceab3237784ff38efab7887e2
piyush008-beep <almightypush008@icloud.com>
2025-11-22 23:49:47 +0530

    done

---
+++ day91.c
@@ -0,0 +1,29 @@
+/*Define a structure Student with name, roll_no, and marks, then read and print one student's data.*/
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int roll_no;
+    float marks;
+};
+
+int main() {
+    struct Student s;
+
+   
+    printf("Enter name: ");
+    fgets(s.name, sizeof(s.name), stdin);
+
+    printf("Enter roll number: ");
+    scanf("%d", &s.roll_no);
+
+    printf("Enter marks: ");
+    scanf("%f", &s.marks);
+
+    
+    printf("Name: %s", s.name);
+    printf("Roll Number: %d\n", s.roll_no);
+    printf("Marks: %f\n", s.marks);
+
+    return 0;
+}
